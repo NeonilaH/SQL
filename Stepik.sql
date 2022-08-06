c99042147596ec5af873f5e8c0028287f737478c
NeonilaH <106426661+NeonilaH@users.noreply.github.com>
2022-08-06 14:08:31 +0200

    Update Stepik.sql

--- Stepik.sql
+++ Stepik.sql
@@ -144,3 +144,10 @@ INSERT INTO book (title, author, price, amount)
     FROM supply
     WHERE author NOT IN ("Булгаков М.А.", "Достоевский Ф.М.");
     
+INSERT INTO book (title, author, price, amount) 
+SELECT title, author, price, amount 
+FROM supply
+WHERE author NOT IN (
+        SELECT author 
+        FROM book
+      );
