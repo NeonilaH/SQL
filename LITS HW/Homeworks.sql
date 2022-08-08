d25cca12437e4e503d98c83420275f8f0f94e67f
NeonilaH <106426661+NeonilaH@users.noreply.github.com>
2022-08-08 10:05:05 +0200

    Update Homeworks.sql

--- LITS HW/Homeworks.sql
+++ LITS HW/Homeworks.sql
@@ -6,25 +6,25 @@
 — Не забуваємо що кожна таблиця повинна містити стовпець ID
 — Можете від себе додати ще таблиці і створити між ними зв’язок
 
-create database cinema;
-create table viewer
+CREATE database cinema;
+CREATE TABLE viewer
 (
-id int (5) auto_increment primary key,
-firstName varchar (15) not null,
-secondName varchar (20) not null
+id INT (5) AUTO_INCREMENT PRIMARY KEY,
+firstName VARCHAR (15) NOT NULL,
+secondName VARCHAR (20) NOT NULL
 );
-create table ticket
+CREATE TABLE ticket
 (
-ticketID int (5) auto_increment primary key,
-movieSession date not null,
-sessionTime time not null,
-hallNumber int (2) not null,
-hallDescription varchar (255) null,
-movieName varchar (255) not null,
-price float not null,
-seat int (2) not null,
-rowNumber int (2) not null,
-foreign key (ticketID) references viewer (id)
+ticketID INT (5) AUTO_INCREMENT PRIMARY KEY,
+movieSession DATE NOT NULL,
+sessionTime TIME NOT NULL,
+hallNumber INT (2) NOT NULL,
+hallDescription VARCHAR (255) NOT NULL,
+movieName VARCHAR (255) NOT NULL,
+price float NOT NULL,
+seat INT (2) NOT NULL,
+rowNumber INT (2) NOT NULL,
+FOREIGN KEY (ticketID) REFERENCES viewer (id)
 );
 
 2. Необхідно встановити БД Bank, а також БД World(якщо вона у Вас відсутня) і написати розв’язок до наступних завдань (всі вони до БД Bank):
@@ -34,11 +34,11 @@ foreign key (ticketID) references viewer (id)
 — Вибрати клієнтів з середньою освітою і місто не Львів.
 — Вибрати клієнтів в яких вік від 29 до 30 років і департамент не 4
 
-SELECT * FROM bank.Client where city = "lviv";
-SELECT firstname, lastname FROM bank.Client where age = "21";
-SELECT * FROM bank.Client where education = "high";
-SELECT * FROM bank.Client where education = "middle" and not city = 'lviv';
-SELECT * FROM bank.Client where age >= "29" and age <= "30" and not Department_idDepartment = "4";
+SELECT * FROM bank.Client WHERE city = "lviv";
+SELECT firstname, lastname FROM bank.Client WHERE age = "21";
+SELECT * FROM bank.Client WHERE education = "high";
+SELECT * FROM bank.Client WHERE education = "middle" AND NOT city = 'lviv';
+SELECT * FROM bank.Client WHERE age >= "29" AND age <= "30" AND NOT Department_idDepartment = "4";
 
 3.  Необхідно написати розв’язок до наступних завдань (всі вони до БД Bank):
 — Вибрати усіх клієнтів, в якого id департаменту є 1 або 2 або 4.
@@ -47,11 +47,11 @@ SELECT * FROM bank.Client where age >= "29" and age <= "30" and not Department_i
 — Вибрати клієнтів в яких прізвище має iv за допомогою regexp.
 — Вибрати усіх клієнтів в яких вік менше 25 і 2-га цифра в паспорті є 5.
 
-SELECT * FROM bank.Client where Department_idDepartment in ("1", "2", "4");
-SELECT * FROM bank.Client where Age not between 30 and 35;
-SELECT * FROM bank.Client where LastName like "%iv";
-SELECT * FROM bank.Client where LastName regexp "iv";
-SELECT * FROM bank.Client where Age < 25 and Passport like "___5%";
+SELECT * FROM bank.Client WHERE Department_idDepartment IN ("1", "2", "4");
+SELECT * FROM bank.Client WHERE Age NOT BETWEEN 30 AND 35;
+SELECT * FROM bank.Client WHERE LastName LIKE "%iv";
+SELECT * FROM bank.Client WHERE LastName regexp "iv";
+SELECT * FROM bank.Client WHERE Age < 25 AND Passport LIKE "___5%";
 
 4. Необхідно написати розв’язок до наступних завдань (всі вони до БД Bank):
 — Вивести всі дані про клієнтів банку посортувавши їх по віку від найменшого до найбільного.
@@ -60,11 +60,11 @@ SELECT * FROM bank.Client where Age < 25 and Passport like "___5%";
 — Вивести всі дані про наймолодшого клієнта банку.
 — Вивести всі дані про клієнтів в яких вік є більшим чим середньостатистичний.
 
-SELECT * FROM bank.Client order by age asc;
-SELECT * FROM bank.Client where city = "Kyiv" order by lastname desc;
-SELECT * FROM bank.Client order by age desc limit 3;
-SELECT * FROM bank.Client where age = (select min(age) from bank.client);
-SELECT * FROM bank.Client where age > (select avg(age) from bank.Client);
+SELECT * FROM bank.Client ORDER BY age ASC;
+SELECT * FROM bank.Client WHERE city = "Kyiv" ORDER BY lastname DESC;
+SELECT * FROM bank.Client ORDER BY age DESC LIMIT 3;
+SELECT * FROM bank.Client WHERE age = (SELECT min(age) FROM bank.client);
+SELECT * FROM bank.Client WHERE age > (SELECT avg(age) FROM bank.Client);
 
 5. Необхідно написати розв’язок до наступних завдань (всі вони до БД Bank):
 — Вивести імена клієнтів та їхні паспортні данні, погрупувавши їх за іменами.
@@ -74,28 +74,29 @@ SELECT * FROM bank.Client where age > (select avg(age) from bank.Client);
 — Визначити найбільший та найменший кредити.
 
 SELECT FirstName, passport FROM bank.Client
-order by FirstName;
+ORDER BY FirstName;
 
-select * FROM bank.Client
-inner join bank.Application
-on Department_idDepartment = Client_idClient
-where sum > 5000;
+SELECT * FROM bank.Client
+INNER JOIN bank.Application
+ON Department_idDepartment = Client_idClient
+WHERE sum > 5000;
 
-select count(LastName) 
-from bank.Client
-union all
-select count(LastName) 
-from bank.Client
-inner join bank.Department
-on idDepartment = Department_idDepartment 
-where DepartmentCity = "Lviv";
+SELECT COUNT(LastName) 
+FROM bank.Client
+UNION all
+SELECT COUNT(LastName) 
+FROM bank.Client
+INNER JOIN bank.Department
+ON idDepartment = Department_idDepartment 
+WHERE DepartmentCity = "Lviv";
 
-select sum, Client_idClient
-from bank.Application
-left join bank.Client
-on Department_idDepartment = Client_idClient
-and sum = (select max(sum) from bank.Application
-where Client_idClient);
+SELECT sum, Client_idClient
+FROM bank.Application
+LEFT JOIN bank.Client
+ON Department_idDepartment = Client_idClient
+AND sum = (SELECT MAX(sum) 
+           FROM bank.Application
+           WHERE Client_idClient);
 
-select min(sum), max(sum)
-from bank.Application;
+SELECT MIN(sum), MAX(sum)
+FROM bank.Application;
