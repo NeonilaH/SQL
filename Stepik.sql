e5a5f2c268e8254f4cb860232c419330ee3709d9
NeonilaH <106426661+NeonilaH@users.noreply.github.com>
2022-08-09 08:50:39 +0200

    Update Stepik.sql

--- Stepik.sql
+++ Stepik.sql
@@ -16,7 +16,6 @@ VALUES (2, "Белая гвардия", "Булгаков М.А.", 540.50, 5),
         (3, "Идиот", "Достоевский Ф.М.", 460.00, 10), 
         (4, "Братья Карамазовы", "Достоевский Ф.М.", 799.01, 2);
 
-
 -- 1.2
 
 SELECT * FROM book;
@@ -208,3 +207,64 @@ SELECT city, COUNT(city) AS Количество
 FROM trip
 GROUP BY city
 LIMIT 2;
+
+SELECT name, city, DATEDIFF(date_last, date_first) + 1 AS Длительность
+FROM trip
+WHERE city != "Москва" AND city != "Санкт-Петербург"
+ORDER BY 3 DESC, 2 DESC;
+
+SELECT name, city, date_first, date_last
+FROM trip
+WHERE DATEDIFF(date_last, date_first) = 
+    (SELECT MIN(DATEDIFF(date_last, date_first)) FROM trip);
+
+SELECT name, city, date_first, date_last
+FROM trip
+WHERE MONTH(date_first) = MONTH(date_last)
+ORDER BY 2, 1;
+
+SELECT MONTHNAME(date_first) AS Месяц, 
+    COUNT(MONTH(date_first)) AS Количество
+FROM trip
+GROUP BY 1
+ORDER BY 2 DESC, 1;
+
+SELECT name, city, date_first, 
+    (DATEDIFF(date_last, date_first) + 1) * per_diem AS Сумма
+FROM trip
+WHERE MONTHNAME(date_first) IN ("February", "March")
+ORDER BY 1, 4 DESC;
+
+SELECT name, SUM((DATEDIFF(date_last, date_first) + 1) * per_diem) AS Сумма
+FROM trip
+GROUP BY name
+HAVING COUNT(name) > 3
+ORDER BY 2 DESC;
+
+-- 1.7
+
+CREATE TABLE fine (
+    fine_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30),
+    number_plate VARCHAR(6),
+    violation VARCHAR(50),
+    sum_fine FLOAT(8,2),
+    date_violation DATE,
+    date_payment DATE
+);
+
+INSERT INTO fine (name, number_plate, violation, sum_fine, date_violation, date_payment)
+    VALUES ("Баранов П.Е.", "Р523ВТ", "Превышение скорости(от 40 до 60)", NULL, "2020-02-14", NULL),
+          ("Абрамова К.А.", "О111АВ", "Проезд на запрещающий сигнал", NULL, "2020-02-23", NULL),
+          ("Яковлев Г.Р.", "Т330ТТ", "Проезд на запрещающий сигнал", NULL, "2020-03-03", NULL);
+
+UPDATE fine f, traffic_violation AS tv
+SET f.sum_fine=tv.sum_fine
+WHERE f.sum_fine IS NULL AND f.violation=tv.violation;
+
+SELECT name, number_plate, violation
+FROM fine
+GROUP BY name, number_plate, violation
+HAVING COUNT(name) AND COUNT(number_plate) AND COUNT(violation) >=2 
+        --HAVING COUNT(*)>=2 - alternatively
+ORDER BY 1, 2, 3;
