aca6b33f5dd3a5d95c78421e3828c3559f78b5a1
NeonilaH <106426661+NeonilaH@users.noreply.github.com>
2022-08-10 09:51:49 +0200

    Update Stepik.sql

--- Stepik.sql
+++ Stepik.sql
@@ -294,3 +294,48 @@ WHERE date_payment IS NULL;
 
 DELETE FROM fine
 WHERE date_violation < "2020-02-01";
+
+
+-- ___________________________________________________________
+
+--2.1
+
+CREATE TABLE author(
+    author_id INT PRIMARY KEY AUTO_INCREMENT,
+    name_author	VARCHAR(50));
+
+INSERT INTO author (name_author)
+VALUES ("Булгаков М.А."),
+        ("Достоевский Ф.М."),
+        ("Есенин С.А."),
+        ("Пастернак Б.Л.");
+
+CREATE TABLE book (
+    book_id INT PRIMARY KEY AUTO_INCREMENT, 
+    title VARCHAR(50), 
+    author_id INT NOT NULL, 
+    genre_id INT,
+    price DECIMAL(8,2), 
+    amount INT, 
+    FOREIGN KEY (author_id) REFERENCES author (author_id),
+    FOREIGN KEY (genre_id) REFERENCES genre (genre_id)
+    );
+
+CREATE TABLE book (
+    book_id INT PRIMARY KEY AUTO_INCREMENT, 
+    title VARCHAR(50), 
+    author_id INT NOT NULL, 
+    genre_id INT,
+    price DECIMAL(8,2), 
+    amount INT, 
+    FOREIGN KEY (author_id) REFERENCES author (author_id) ON DELETE CASCADE,
+    FOREIGN KEY (genre_id) REFERENCES genre (genre_id) ON DELETE SET NULL
+    );
+
+INSERT INTO book (title, author_id, genre_id, price, amount)
+VALUES ('Стихотворения и поэмы',3, 2, 650.00, 15),
+        ('Черный человек', 3, 2, 570.20, 6),
+        ('Лирика', 4, 2, 518.99, 2)
+
+-- 2.2
+
