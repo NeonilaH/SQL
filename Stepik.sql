8b3f9b9e0e3420d3166134885cacc03b4aef944b
NeonilaH <106426661+NeonilaH@users.noreply.github.com>
2022-08-11 12:09:24 +0200

    Update Stepik.sql

--- Stepik.sql
+++ Stepik.sql
@@ -379,6 +379,7 @@ FROM book
     INNER JOIN  author ON author.author_id  = book.author_id
 GROUP BY name_author 
 HAVING COUNT(DISTINCT book.genre_id) = 1; 
+
 SELECT title, name_author, name_genre, price, amount
 FROM book JOIN author ON book.author_id = author.author_id
           JOIN genre ON book.genre_id = genre.genre_id
