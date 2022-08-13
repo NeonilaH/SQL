a552c789ca330f359dc7e88118f65fadc27181b0
NeonilaH <106426661+NeonilaH@users.noreply.github.com>
2022-08-13 16:07:07 +0200

    Update Stepik.sql

--- Stepik.sql
+++ Stepik.sql
@@ -513,3 +513,13 @@ FROM buy_step
     INNER JOIN client USING (client_id)
     INNER JOIN city USING (city_id)
 WHERE step.name_step = "Транспортировка" and buy_step.date_step_end IS NOT NULL;
+
+SELECT DISTINCT name_client
+FROM client
+    INNER JOIN buy USING (client_id)
+    INNER JOIN buy_book USING (buy_id)
+    INNER JOIN book USING (book_id)
+    INNER JOIN author USING (author_id)
+WHERE name_author LIKE "Достоевск%"
+ORDER BY 1;
+
