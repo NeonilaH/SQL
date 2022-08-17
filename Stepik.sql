0aced27e0372acffea85b7dc43d8cbbfedaa3a09
NeonilaH <106426661+NeonilaH@users.noreply.github.com>
2022-08-17 17:32:01 +0200

    Update Stepik.sql

--- Stepik.sql
+++ Stepik.sql
@@ -605,3 +605,16 @@ WHERE buy_id = 5;
 UPDATE buy_step
 SET date_step_beg = "2020-04-12"
 WHERE buy_id = 5 and step_id = 1;
+
+UPDATE buy_step
+SET date_step_end = "2020-04-13"
+WHERE buy_id = 5 AND step_id = (
+    SELECT step_id 
+    FROM step
+    WHERE name_step = "Оплата"), 
+UPDATE buy_step
+SET date_step_beg = "2020-04-13"
+WHERE buy_id = 5 AND step_id = (
+    SELECT step_id + 1 
+    FROM step
+    WHERE name_step = "Оплата");
