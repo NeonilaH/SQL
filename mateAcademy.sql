f166881941c99d0afb39aace3642e1451114d08a
NeonilaH <106426661+NeonilaH@users.noreply.github.com>
2022-08-30 09:40:23 +0200

    Update mateAcademy.sql

--- mateAcademy.sql
+++ mateAcademy.sql
@@ -1,10 +1,10 @@
-/*1. Display information about all existing dishes in the format: [dish name]: [price] UAH
+/* 1. Display information about all existing dishes in the format: [dish name]: [price] UAH
 Set a temporary name for the resulting column - dish_information.*/
 
 SELECT CONCAT (name, ': ', price, ' UAH') AS dish_information
 FROM dish;
 
-/*2. Display the number of new staff who were hired each year.
+/* 2. Display the number of new staff who were hired each year.
 The result should contain the following columns:
 the year from date_of_hiring: year
 number of staff that were hired that year: number_of_staff*/
@@ -14,6 +14,19 @@ COUNT(id) AS number_of_staff
 FROM staff
 GROUP BY year;
 
-/*3. Display the number of students who have a birthday in the current calendar month. 
+/* 3. Display the number of students who have a birthday in the current calendar month. 
 Set a temporary name for the resulting column - number_of_pupils.*/
+
+SELECT COUNT(id) AS number_of_pupils
+FROM pupil
+WHERE EXTRACT(MONTH FROM CURRENT_DATE) = EXTRACT(MONTH FROM birth_date)
+
+/* 4.The tables category and product are given.
+Display information about each of the products of the category 'Beverages' in the form: [product name] - [amount] units.
+Set a temporary name for the resulting column - product_information. */
+
+SELECT CONCAT(p.name, ' - ', amount, ' units') AS product_information
+FROM category c
+    JOIN product p ON c.id = p.category_id
+WHERE c.name = 'Beverages';
 
