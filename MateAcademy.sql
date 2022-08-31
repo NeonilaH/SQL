dfce85d2e65a64dd38405e492f6b1fb7310c4fbf
NeonilaH <106426661+NeonilaH@users.noreply.github.com>
2022-08-31 13:05:35 +0200

    Update MateAcademy.sql

--- MateAcademy.sql
+++ MateAcademy.sql
@@ -1,5 +1,5 @@
 /* 1. Display information about all existing dishes in the format: [dish name]: [price] UAH
-Set a temporary name for the resulting column - dish_information.*/
+Set a temporary name for the resulting column - dish_information. */
 
 SELECT CONCAT(name, ': ', price, ' UAH') AS dish_information
 FROM dish;
@@ -7,7 +7,7 @@ FROM dish;
 /* 2. Display the number of new staff who were hired each year.
 The result should contain the following columns:
 the year from date_of_hiring: year
-number of staff that were hired that year: number_of_staff*/
+number of staff that were hired that year: number_of_staff */
 
 SELECT EXTRACT (YEAR FROM date_of_hiring) AS year, 
 COUNT(id) AS number_of_staff
