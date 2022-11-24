4518117fc5f4c9e3796cd6fc3b41f10284906408
NeonilaH <106426661+NeonilaH@users.noreply.github.com>
2022-11-24 12:18:21 +0100

    Update SoloLearn.sql

--- SoloLearn.sql
+++ SoloLearn.sql
@@ -1,15 +1,11 @@
 ALTER TABLE cities
 ADD AttractivePlace VARCHAR(128);
-
 UPDATE cities
 SET AttractivePlace = 'Belem Tower' WHERE name = 'Lisbon';
-
 UPDATE cities
 SET  AttractivePlace = 'Plaza Mayor' WHERE name = 'Madrid';
-
 UPDATE cities
 SET AttractivePlace = 'Eiffel Tower' WHERE name = 'Paris';
-
 SELECT * FROM cities;
 
 RENAME TABLE people
